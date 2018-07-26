43de4eb48564d704b793180ac35ad8b263d72ce8
Anthony Kumasaka <anthonykumasaka@gmail.com>
2018-07-26 01:40:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// add solution here
+function theBeatlesPlay(arrMusicians, arrInstruments) {
+  var emptyArray = [];
+  for (var i = 0; i < arrMusicians.length; i++){
+    emptyArray[i] = arrMusicians[i] + " plays " + arrInstruments[i]
+    } return emptyArray; 
+}
+
+function johnLennonFacts(array){
+  var i = 0; 
+  while (i < array.length) {
+    array[i] = array[i] + "!!!";
+  } return array 
+}
